eb3f6709e7ab684817e6881fde403e1686d3a56e
黄浩 <1348666438@qq.com>
2019-03-20 11:52:29 +0800

    添加view

---
+++ GitTest/CustomView.h
@@ -0,0 +1,17 @@
+//
+//  CustomView.h
+//  GitTest
+//
+//  Created by 黄浩 on 2019/3/20.
+//  Copyright © 2019年 黄浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
